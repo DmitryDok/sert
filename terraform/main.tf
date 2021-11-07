4f485d6f378a09b26eefc02acb20e82aaaa13d99
profiles <dmitry@mail.ru>
2021-11-07 13:58:43 +0300

    boxfuse rc2 fix5

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,28 +22,50 @@ resource "aws_key_pair" "deploer_key" {
 resource "aws_security_group" "ssh_allow" {
   name        = "ssh_allow_sg"
   description = "Allow SSH"
-  ingress {
+  ingress = [
+    {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+    }
+  ]
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
 }
 resource "aws_security_group" "tomcat_allow" {
   name        = "tomcat_allow_sg"
-  description = "Allow SSH"
-  ingress {
+  description = "Allow Tomcat"
+  ingress = [
+    {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
+    }
+    {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+    }
+  ]
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
 }
 resource "aws_instance" "build" {
   ami = var.ami
