3e55f57a115a9c39ec94b811c69ef30df2eb3286
profiles <dmitry@mail.ru>
2021-11-07 13:05:33 +0300

    boxfuse rc2 fix1

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,10 +19,31 @@ resource "aws_key_pair" "deploer_key" {
   key_name = "deploer"
   public_key = file("~/.ssh/id_rsa.pub")
 }
+resource "aws_security_group" "ssh_allow" {
+  name        = "ssh_allow"
+  description = "Allow SSH"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "tomcat_allow" {
+  name        = "ssh_allow"
+  description = "Allow SSH"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "build" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = aws_key_pair.deploer_key.key_name
+  vpc_security_group_ids = [aws_security_group.ssh_allow.id]
   provisioner "local-exec" {
     command = "echo [build] >> ./ansible/hosts && echo ${aws_instance.build.public_ip} >> ./ansible/hosts"
   }
@@ -31,6 +52,7 @@ resource "aws_instance" "prod" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = aws_key_pair.deploer_key.key_name
+  vpc_security_group_ids = [aws_security_group.ssh_allow.id],[aws_security_group.tomcat_allow.id]
   provisioner "local-exec" {
     command = "echo [prod] >> ./ansible/hosts && echo ${aws_instance.prod.public_ip} >> ./ansible/hosts"
   }
